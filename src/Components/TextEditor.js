654b0350cc7613f7a750b9edcb5fb809f6a68a3d
Nicholas Trienens <nick@fuzzproductions.com>
2018-10-09 17:01:50 -0600

    [simpler is sometimes better]

--- src/Components/TextEditor.js
+++ src/Components/TextEditor.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import EditableLabel from 'react-inline-editing';
 import EditableField from './EditableField.js';
+import { setContent} from '../Services/firebase.js';
 
 export default class TextEditor extends React.Component {
 
@@ -11,7 +14,21 @@ export default class TextEditor extends React.Component {
       contentKey: props.contentKey,
       createRow: props.createRow
     }
-  }
+    this._handleFocus = this._handleFocus.bind(this);
+  this._handleFocusOut = this._handleFocusOut.bind(this);
+}
+
+_handleFocus(text) {
+    const { data, contentKey, createRow } = this.state;
+    console.log('Focused with text: ' + text);
+
+}
+
+_handleFocusOut(text) {
+    const { data, contentKey, createRow } = this.state;
+    console.log('Left editor with text: ' + text);
+    setContent(contentKey, text)
+}
 
   componentWillReceiveProps(nextProps) {
     if (this.state.contentKey === nextProps.contentKey) {
@@ -34,17 +51,30 @@ export default class TextEditor extends React.Component {
     if ( data !== null && data !== undefined ){
         text  = data.text;
     }
-    return <EditableField
-                text={text}
-                className="story-title"
-                contentKey={contentKey}
-                createRow={createRow}
-                submitCallback={ (text, success, failure) => {
 
-                  success();
+    return <EditableLabel text={text}
+               labelClassName='myLabelClass'
+               inputClassName='myInputClass'
+               inputWidth='300px'
+               inputHeight='35px'
+               inputMaxLength='50'
+               labelFontWeight='bold'
+               inputFontWeight='bold'
+               onFocus={this._handleFocus}
+               onFocusOut={this._handleFocusOut}
+           />
 
-              }
-           }
-        />
+    // return <EditableField
+    //             text={text}
+    //             className="story-title"
+    //             contentKey={contentKey}
+    //             createRow={createRow}
+    //             submitCallback={ (text, success, failure) => {
+    //
+    //               success();
+    //
+    //           }
+    //        }
+    //     />
   }
 }
